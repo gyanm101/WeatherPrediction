dd93fe3146c03c93332cb226554bfbd6fa3df241
gyanm101 <64622452+gyanm101@users.noreply.github.com>
2025-02-19 16:26:22 -0500

    Debugging port issues

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 1000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
